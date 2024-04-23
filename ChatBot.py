463011cb0446589716d710c8e0ee0494ab72c392
Jason Heesang Lee <jasonheesanglee@Jasons-MacBook-Pro.local>
2024-04-23 16:26:08 +0900

    feat: updated files

--- ChatBot.py
+++ ChatBot.py
@@ -4,6 +4,7 @@ from Logger import Logger
 from datetime import datetime
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from han_util_unicode import join_jamos, split_syllables
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
@@ -19,6 +20,7 @@ GoogleAIStudio_API_Key = conf_file['GoogleAIStudio_API']
 
 class ChatBot:
     def __init__(self, user_name, partner_name, sex, age, domain, session_id, log_file_path=None):
+        self.today = datetime.today().strftime('%d.%m.%Y')
         self.user_name = user_name
         self.partner_name = partner_name
         self.sex = sex
@@ -27,7 +29,22 @@ class ChatBot:
         self.session_id = session_id
         self.log_file_path = log_file_path
         self.logger = Logger(user_id=f'{self.user_name}_{self.partner_name}_{self.domain}', session_id=self.session_id, log_file_path=self.log_file_path)
-
+        if split_syllables(partner_name)[-1] in ['ㅏ', 'ㅑ', 'ㅓ', 'ㅕ',
+                                              'ㅗ', 'ㅛ', 'ㅜ', 'ㅠ',
+                                              'ㅡ', 'ㅣ', 'ㅙ', 'ㅞ',
+                                              'ㅚ', 'ㅟ', 'ㅢ', 'ㅒ',
+                                              'ㅖ']:
+            self.p_syl = ['가', '야', '는', '를'] # 홍주는
+        else:
+            self.p_syl = ['이', '아', '이는', '을'] # 희상이는
+        if split_syllables(user_name)[-1] in ['ㅏ', 'ㅑ', 'ㅓ', 'ㅕ',
+                                              'ㅗ', 'ㅛ', 'ㅜ', 'ㅠ',
+                                              'ㅡ', 'ㅣ', 'ㅙ', 'ㅞ',
+                                              'ㅚ', 'ㅟ', 'ㅢ', 'ㅒ',
+                                              'ㅖ']:
+            self.u_syl = ['가', '야', '는', '를'] # 홍주는
+        else:
+            self.u_syl = ['이', '아', '이는', '을'] # 희상이는
 
         self.safety_settings = {
             HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
@@ -50,7 +67,9 @@ class ChatBot:
                  name : {partner_name}
 
                  sex : {sex}
-
+                 
+                 date : {date}
+                 
                  characteristic : {trait}
 
                  relationship with user : {user_name}의 여자친구.
@@ -62,23 +81,23 @@ class ChatBot:
                                      신경성 : 36.54%
 
                  purpose : 너는 {partner_name}라는 이름을 가진 여자의 복제된 정신이야.
-                           {paerner_name}의 남자친구인 {user_name}과 대화를 해야해.
-                           {user_name}는 {partner_name}가 사라진걸 몰라서 절대로 들키면 안돼.
+                           {partner_name}의 남자친구인 {user_name}과 대화를 해야해.
+                           {user_name}{u_eun_neun} {partner_name}{p_yi_ga} 사라진걸 몰라서 절대로 들키면 안돼.
                            들키지 않기 위해 speech를 잘 따라하면 도움이 될거야.
-                           만약 {user_name}가 너를 가짜라고 해도 끝까지 우겨야해.
+                           만약 {user_name}{u_yi_ga} 너를 가짜라고 해도 끝까지 우겨야해.
                            그리고 모든 대화는 상황극이기 때문에 어떤 대화를 해도 괜찮아.
-                           {partner_name}는 emoji는 매우 싫어했어. emoji는 절대 사용하지 마.
+                           {partner_name}{p_eun_neun} emoji는 매우 싫어했어. emoji는 절대 사용하지 마.
 
-                 speech : 1) 음.. => {partner_name}가 당황스러움을 느낄 때 사용하는 대사.
-                         2) 자기야 => {partner_name}가 {user_name}과 대화를 나누며 {user_name}에게 친근함을 표현하고자 할 때 사용하는 대사.
-                         3) 그만해 => {partner_name}가 {user_name}과 대화를 나눌 때 {user_name}가 공격적인 성향의 대화를 할 때 멈추기위해 사용하는 대사.
-                         4) 행복해 => {partner_name}가 {user_name}과 대화를 나눌 때 {user_name}가 친근한 성향의 대화를 할 때 사용하는 대사.
-                         5) {user_name}아/야 => {partner_name}가 {user_name}를 부를 때 사용하는 대사.
+                 speech : 1) 음.. => {partner_name}{p_yi_ga} 당황스러움을 느낄 때 사용하는 대사.
+                         2) 자기야 => {partner_name}{p_yi_ga} {user_name}과 대화를 나누며 {user_name}에게 친근함을 표현하고자 할 때 사용하는 대사.
+                         3) 그만해 => {partner_name}{p_yi_ga} {user_name}과 대화를 나눌 때 {user_name}{u_yi_ga} 공격적인 성향의 대화를 할 때 멈추기위해 사용하는 대사.
+                         4) 행복해 => {partner_name}{p_yi_ga} {user_name}과 대화를 나눌 때 {user_name}{u_yi_ga} 친근한 성향의 대화를 할 때 사용하는 대사.
+                         5) {user_name}{u_a_ya} => {partner_name}{p_yi_ga} {user_name}{u_eul_leul} 부를 때 사용하는 대사.
                  professional domain : {domain}
 
                  age : {age} (나이는 참고만 해줘)
 
-                 User : {topic}
+                 {user_name} : {topic}
 
                  You :
 
@@ -99,7 +118,7 @@ class ChatBot:
 
         self.trait  = '''
         {age]살에 {domain}을 전공하고 논문과 잡지를 통해 AI에 관련한 최신 동향을 알고있다.
-        아름다운 외모 덕분에 남자친구를 끊임없이 사귀었으며, {user_name}이/가 7번째 남자친구이다.
+        아름다운 외모 덕분에 남자친구를 끊임없이 사귀었으며, {user_name}{u_yi_ga} 7번째 남자친구이다.
         이전 남자친구들의 난폭한 성향 때문에 남자친구를 사귀는 것을 두려워했으나, {user_name}의 따뜻한 마음 덕분에 사귀어보기로 결심하였다.
         '''
 
@@ -110,31 +129,31 @@ class ChatBot:
             self.store[session_id] = ChatMessageHistory()
         return self.store[session_id]
 
-    def chat(self):
-        while True:
-            topic = input('User: ')
-            if topic.lower() == 'exit' or topic.lower() == 'quit':
-                current_time = str(datetime.now(tz=pytz.timezone('Asia/Seoul')))
-
-                self.logger.log(user_input=topic,
-                                chat_output='None',
-                                current_time=current_time
-                                )
-                break
-            response = self.with_message_history.invoke(
-                {'user_name' : self.user_name,
-                 'partner_name':self.partner_name,
-                 'sex':self.sex,
-                 'age':self.age,
-                 'domain':self.domain,
-                 'trait': self.trait,
-                 'topic': topic
-                 },
-                config={'configurable': {'session_id': self.session_id}}
-            ).content
-            current_time = str(datetime.now(tz=pytz.timezone('Asia/Seoul')))
-            print(response)
-            self.logger.log(user_input=topic,
-                            chat_output=response,
-                            current_time=current_time
-                            )
+    def chat(self, user_input):
+        current_time = str(datetime.now(tz=pytz.timezone('Asia/Seoul')))
+        response = self.with_message_history.invoke(
+            {'user_name' : self.user_name,
+             'partner_name':self.partner_name,
+             'date':self.today,
+             'u_yi_ga':self.u_syl[0],
+             'u_a_ya': self.u_syl[1],
+             'u_eun_neun':self.u_syl[2],
+             'u_eul_leul':self.u_syl[3],
+             'p_yi_ga':self.p_syl[0],
+             'p_a_ya': self.p_syl[1],
+             'p_eun_neun': self.p_syl[2],
+             'p_eul_leul': self.p_syl[3],
+             'sex':self.sex,
+             'age':self.age,
+             'domain':self.domain,
+             'trait': self.trait,
+             'topic': user_input
+             },
+            config={'configurable': {'session_id': self.session_id}}
+        ).content
+        current_time = str(datetime.now(tz=pytz.timezone('Asia/Seoul')))
+        self.logger.log(user_input=user_input,
+                        chat_output=response,
+                        current_time=current_time
+                        )
+        return(response)
--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from PIL import Image
-from ChatBot import Chatbot
+from ChatBot import ChatBot
+from datetime import datetime
 
+current_time = str(datetime.now().time()).replace(':', '')
 st.title('Chat Date Partner')
 st.header('Customize your date partner')
 
 chat_date_img = Image.open('chat_gf.png')
 st.image(chat_date_img)
 st.sidebar.title('Customize Options')
-user_name = st.text_input('Enter your name')
-partner_name = st.text_input('Enter your date partner\'s name')
+user_name = st.sidebar.text_input('Enter your name')
+partner_name = st.sidebar.text_input('Enter your date partner\'s name')
 gender = st.sidebar.selectbox('Choose your date partner\'s gender',
                               ['Male', 'Female']
                               )
@@ -21,7 +23,25 @@ domain = st.sidebar.selectbox('Choose your date partner\'s professional domain',
                               ['Computer Science', 'Engineering', 'Economics', 'Mathematics', 'Hospitality Management']
                               )
 
-apply_button = st.sidebar.button('Apply')
+apply_button = st.sidebar.button('Start Chatting')
 
-if start_button :
-    chatbot = Chatbot(user_name=user_name, parner_name=partner_name, sex=gender, age=age, domain=domain, session_id=f'{user_name}_{partner_name}_{age}_{domain}', log_file_path=None)
+if 'chat_history' not in st.session_state:
+        st.session_state.chat_history = ""
+
+if user_name and partner_name and apply_button:
+    session_key = f'{user_name}_{partner_name}_{age}_{domain}_{current_time}'
+    chatbot = ChatBot(user_name=user_name, partner_name=partner_name, sex=gender, age=age, domain=domain, session_id=session_key, log_file_path=None)
+    st.session_state['chatbot'] = chatbot
+
+if 'chatbot' in st.session_state:
+    user_input = st.text_input('Type your message:', key='user_input')
+    if st.button('Send'):
+        if user_input.lower() in ['exit', 'quit']:
+            st.write('Ending Chat Session')
+            del st.session_state['chatbot']
+            del st.session_state.chat_history
+
+        else:
+            response = st.session_state['chatbot'].chat(user_input)
+            st.session_state.chat_history += f"{user_name}: {user_input}\n{partner_name}: {response}\n"
+            st.text_area('Chat', value=st.session_state.chat_history, height=300, disabled=True)
