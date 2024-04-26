6d8ce356a962faae831faaa147806f8303cf9500
Jason Heesang Lee <jasonheesanglee@Jasons-MacBook-Pro.local>
2024-04-26 11:47:28 +0900

    feat: updating to fix error

--- app.py
+++ app.py
@@ -103,61 +103,77 @@ def send_message(input_text, user_name, partner_name):
                 'text': response
             })
 
+st.st_page_config('Chat Date Partner', page_icon='😍')
+st.title('Chat Date Partner')
+st.header('개인화된 연인과 대화를 나누어보세요!')
 
-def main():
-    st.title('Chat Date Partner')
-    st.header('개인화된 연인과 대화를 나누어보세요!')
+chat_date_img = Image.open('chat_gf.png')
+width, height = chat_date_img.size
+chat_date_img = chat_date_img.resize((width, height))
 
-    chat_date_img = Image.open('chat_gf.png')
-    width, height = chat_date_img.size
-    chat_date_img = chat_date_img.resize((width, height))
+left_co, cent_co, last_co = st.columns(3)
+with cent_co:
+    st.image(chat_date_img)
 
-    left_co, cent_co, last_co = st.columns(3)
-    with cent_co:
-        st.image(chat_date_img)
+st.sidebar.title('내 연인 설정하기')
+user_name = st.sidebar.text_input('이름/닉네임을 입력해주세요')
+partner_name = st.sidebar.text_input('연인의 이름/닉네임을 입력해주세요')
+gender = st.sidebar.selectbox('연인의 성별을 골라주세요.', ['여자', '남자'])
+age = st.sidebar.slider('연인의 나이를 설정해주세요.', 21, 100, value=26)
+domain = st.sidebar.selectbox('연인의 전공을 골라주세요.', ['컴퓨터공학', '기계공학', '경제학', '수학', '호텔경영학'])
+gaebang = sidebar_slider('개방성', value=84.40)
+seongsil = sidebar_slider('성실성', value=92.91)
+woehyang = sidebar_slider('외향성', value=90.43)
+chinhwa = sidebar_slider('친화성', value=88.65)
+singyung = sidebar_slider('신경성', value=63.48)
 
-    st.sidebar.title('내 연인 설정하기')
-    user_name = st.sidebar.text_input('이름/닉네임을 입력해주세요')
-    partner_name = st.sidebar.text_input('연인의 이름/닉네임을 입력해주세요')
-    gender = st.sidebar.selectbox('연인의 성별을 골라주세요.', ['여자', '남자'])
-    age = st.sidebar.slider('연인의 나이를 설정해주세요.', 21, 100, value=26)
-    domain = st.sidebar.selectbox('연인의 전공을 골라주세요.', ['컴퓨터공학', '기계공학', '경제학', '수학', '호텔경영학'])
-    gaebang = sidebar_slider('개방성', value=84.40)
-    seongsil = sidebar_slider('성실성', value=92.91)
-    woehyang = sidebar_slider('외향성', value=90.43)
-    chinhwa = sidebar_slider('친화성', value=88.65)
-    singyung = sidebar_slider('신경성', value=63.48)
+apply_button = st.sidebar.button('연인과의 챗 시작하기')
 
-    apply_button = st.sidebar.button('연인과의 챗 시작하기')
+# ---------------------------------------------------------------------------------------------------------------------
 
-    if 'chat_history' not in st.session_state:
-        st.session_state.chat_history = []
+if apply_button and user_name and partner_name:
+    current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    session_key = f'{user_name}_{partner_name}_{age}_{domain}_{current_time}'
+    chatbot = ChatBot(user_name=user_name, partner_name=partner_name, sex=gender, age=age, domain=domain,
+                      session_id=session_key, gaebang=gaebang, seongsil=seongsil,
+                      woehyang=woehyang, chinhwa=chinhwa, singyung=singyung, log_file_path=None)
+    st.session_state['chatbot'] = chatbot
 
-    if st.session_state.chat_history == []:
-        if user_name and partner_name and apply_button:
-            current_time = datetime.now().strftime('%Y%m%d%H%M%S')
-            session_key = f'{user_name}_{partner_name}_{age}_{domain}_{current_time}'
-            chatbot = ChatBot(user_name=user_name, partner_name=partner_name, sex=gender, age=age, domain=domain,
-                              session_id=session_key, gaebang=gaebang, seongsil=seongsil,
-                              woehyang=woehyang, chinhwa=chinhwa, singyung=singyung, log_file_path=None)
-            st.session_state['chatbot'] = chatbot
+else:
+    st.warning('연인과의 챗 시작하기 버튼을 눌러주세요.')
 
-        else:
-            st.warning('연인과의 챗 시작하기 버튼을 눌러주세요.')
+with st.form("Chat Form", clear_on_submit=True):
+    user_input = st.text_input("메시지를 입력해주세요:", key="chat_input")
+    submit_button = st.form_submit_button("Send")
+    st.write(user_input)
 
-    with st.form("Chat Form", clear_on_submit=True):
-        user_input = st.text_input("메시지를 입력해주세요:", key="chat_input")
-        submit_button = st.form_submit_button("Send")
-        st.write(user_input)
+def send_message(input_text, user_name, partner_name):
+    if input_text.lower() in ['exit', 'quit']:
+        st.session_state.chat_history.append({
+            'name': user_name,
+            'text': 'Ending Chat Session.'
+        })
+        st.write(input_text)
+        if 'chatbot' in st.session_state:
+            del st.session_state['chatbot']
+    else:
+        if 'chatbot' in st.session_state:
+            response = st.session_state['chatbot'].chat(input_text)
+            st.session_state.chat_history.append({
+                'name': user_name,
+                'text': input_text
+            })
+            st.write(input_text)
+            st.session_state.chat_history.append({
+                'name': partner_name,
+                'text': response
+            })
+            st.write(response)
 
-    if submit_button and user_input:
-        send_message(user_input, user_name, partner_name)
-    for msg in st.session_state.chat_history:
-        # st.chat_message takes a string and automatically handles the display.
-        with st.chat_message('user'):
-            st.write(msg['text'])
+if submit_button and user_input:
+    send_message(user_input, user_name, partner_name)
 
-    # Move the display of chat messages outside the form to ensure they persist
-
-if __name__ == '__main__':
-    main()
+# for msg in st.session_state.chat_history:
+#     # st.chat_message takes a string and automatically handles the display.
+#     with st.chat_message('user'):
+#         st.write(msg['text'])
