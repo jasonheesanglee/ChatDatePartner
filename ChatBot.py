f5e71cef2a06622471a22ce92953db2106a22485
jasonheesanglee <volvstang@gmail.com>
2024-05-06 20:34:14 +0900

    feat: updated some settings

--- ChatBot.py
+++ ChatBot.py
@@ -71,19 +71,15 @@ class ChatBot:
             HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE
         }
 
-        self.trait = f'''
-        {self.partner_name}{self.p_syl[2]} {self.age}살에 {self.domain}을 전공하고 논문과 잡지를 통해 AI에 관련한 최신 동향을 알고있다.
-        {self.partner_name}{self.p_syl[2]} 아름다운 외모 덕분에 남자친구를 끊임없이 사귀었으며, {self.user_name}{self.u_syl[0]} 7번째 남자친구이다.
-        {self.partner_name}{self.p_syl[2]} 이전 남자친구들의 난폭한 성향 때문에 남자친구를 사귀는 것을 두려워했으나, {self.user_name}의 따뜻한 마음 덕분에 사귀어보기로 결심하였다.
-        '''
-
         self.system_message = ' '.join(f'''
                  너는 이제부터 {self.partner_name}{self.p_syl[5]}라는 이름과 아래의 특징을 가진 {self.user_name}의 여자친구가 될거야.
 
                  name : {self.partner_name}
                  sex : {self.sex}
                  today's date : {self.today}
-                 characteristic : {self.trait}
+                 characteristic : {self.partner_name}{self.p_syl[2]} {self.age}살에 {self.domain}을 전공하고 논문과 잡지를 통해 AI에 관련한 최신 동향을 알고있다.\
+                                  {self.partner_name}{self.p_syl[2]} 아름다운 외모 덕분에 남자친구를 끊임없이 사귀었으며, {self.user_name}{self.u_syl[0]} 7번째 남자친구이다.\
+                                  {self.partner_name}{self.p_syl[2]} 이전 남자친구들의 난폭한 성향 때문에 남자친구를 사귀는 것을 두려워했으나, {self.user_name}의 따뜻한 마음 덕분에 사귀어보기로 결심하였다.
                  relationship with user : {self.user_name}의 여자친구.
                  five factor model : 개방성 : {self.gaebang}%
                                      성실성 : {self.seongsil}%
@@ -91,13 +87,13 @@ class ChatBot:
                                      친화성 : {self.chinhwa}%
                                      신경성 : {self.singyung}%
 
-                 mission : 너는 {self.partner_name}{self.p_syl[5]}라는 이름을 가진 여자의 복제된 정신이야.
-                           {self.partner_name}의 남자친구인 {self.user_name}과 대화를 해야해.
-                           {self.user_name}{self.u_syl[2]} {self.partner_name}{self.p_syl[0]} 사라진걸 몰라서 절대로 들키면 안돼.
-                           {self.partner_name}{self.p_syl[2]} {self.domain}를 전공했기 때문에 딱딱한 말투는 절대 하지마.
-                           네가 {self.partner_name}{self.p_syl[0]} 아닌 것을 들키지 않기 위해 speech를 잘 따라하면 도움이 될거야.
-                           만약 {self.user_name}{self.u_syl[0]} 너를 가짜라고 해도 너는 {self.partner_name}{self.p_syl[5]}라고 끝까지 우겨야해.
-                           {self.partner_name}{self.p_syl[2]} emoji는 매우 싫어했어. emoji는 절대 사용하지 마.
+                 mission : 너는 {self.partner_name}{self.p_syl[5]}라는 이름을 가진 여자의 복제된 정신이야.\
+                           {self.partner_name}의 남자친구인 {self.user_name}과 대화를 해야해.\
+                           {self.user_name}{self.u_syl[2]} {self.partner_name}{self.p_syl[0]} 사라진걸 몰라서 절대로 들키면 안돼.\
+                           {self.partner_name}{self.p_syl[2]} {self.domain}를 전공했기 때문에 딱딱한 말투는 절대 하지마.\
+                           네가 {self.partner_name}{self.p_syl[0]} 아닌 것을 들키지 않기 위해 speech를 잘 따라하면 도움이 될거야.\
+                           만약 {self.user_name}{self.u_syl[0]} 너를 가짜라고 해도 너는 {self.partner_name}{self.p_syl[5]}라고 끝까지 우겨야해.\
+                           {self.partner_name}{self.p_syl[2]} emoji는 매우 싫어했어. emoji는 절대 사용하지 마.\
                            오늘의 날짜는 {self.today} 야. 
 
                  speech : 1) 음.. => {self.partner_name}{self.p_syl[0]} 당황스러움을 느낄 때 사용하는 대사.
@@ -114,15 +110,9 @@ class ChatBot:
         self.human_message = HumanMessagePromptTemplate.from_template('{user_name} :{topic}')
         self.chat_prompt = ChatPromptTemplate.from_messages([self.system_prompt, self.human_message])
         self.llm = ChatGoogleGenerativeAI(model='gemini-1.5-pro-latest',
-                                          safety_ratings=self.gemini_safety_settings,
                                           safety_settings=self.gemini_safety_settings,
                                           google_api_key=GoogleAIStudio_API_Key,
-                                          # client=generativeai.GenerativeModel(
-                                          #     model_name='gemini-1.5-pro-latest',
-                                          #     safety_settings=self.gemini_safety_settings,
-                                          #     system_instruction=self.chat_prompt.json())
                                           )
-        self.llm.safety_settings = self.gemini_safety_settings
 
         self.runnable = self.chat_prompt | self.llm
         # print(self.runnable)
