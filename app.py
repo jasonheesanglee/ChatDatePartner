a4fa2148a75abebf022056335510bb12c78d6baf
jasonheesanglee <volvstang@gmail.com>
2024-05-06 15:05:38 +0900

    feat: updaing minor error

--- app.py
+++ app.py
@@ -63,8 +63,9 @@ if user_name and partner_name and apply_button:
 
 if 'chatbot' in st.session_state:
     messages = st.container(height=600)
-    for msg in st.session_state.chat_history:
-        messages.chat(msg[name]).write(text)
+    if st.session_state.chat_history != []:
+        for msg in st.session_state.chat_history:
+            messages.chat(msg[name]).write(text)
     if prompt := st.chat_input('메시지를 입력해주세요 : '):
         messages.chat_message(user_name).write(prompt)
 
