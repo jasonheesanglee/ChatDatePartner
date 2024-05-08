59a03554003d758294421e1ebb6d141660619ee6
jasonheesanglee <volvstang@gmail.com>
2024-05-08 09:09:08 +0900

    feat: updating Logger

--- Logger.py
+++ Logger.py
@@ -7,7 +7,7 @@ class Logger:
         self.session_id = session_id
         self.log_file_path = log_file_path
 
-    def get_log(self):
+    def get_log(self) -> Dict:
         if self.log_file_path == None:
             if not os.path.exists('./logs'):
                 os.mkdir('./logs')
@@ -22,29 +22,37 @@ class Logger:
             logs = {}
         return logs
 
+    def log_message(self, user_input, chat_output, current_time):
+        msg = [
+                    {'role':'USER', 'message': user_input},
+                    {'role':'CHATBOT', 'message': chat_output},
+                    {'role':'SYSTEM','current_time':current_time}
+                ]
+        return msg
+
     def log(self, user_input, chat_output, current_time):
         logs = self.get_log()
         user_session = logs.setdefault(user_input, {}).setdefault(self.session_id, [])
 
         if self.user_id in logs.keys():
             if self.session_id in logs[self.user_id]:
-                logs[self.user_id][self.session_id].append([
-                    {'role':'USER', 'message': user_input},
-                    {'role':'CHATBOT', 'message': chat_output},
-                    {'role':'SYSTEM','current_time':current_time}
-                ])
+                logs[self.user_id][self.session_id].append(
+                    self.log_message(user_input,
+                                     chat_output,
+                                     current_time
+                                     )
+                )
             else:
-                logs[self.user_id].add({self.session_id : [
-                    {'role':'USER', 'message': user_input},
-                    {'role':'CHATBOT', 'message': chat_output},
-                    {'role':'SYSTEM', 'current_time':current_time}
-                ]})
+                logs[self.user_id].add({self.session_id : self.log_message(user_input,
+                                                                           chat_output,
+                                                                           current_time
+                                                                           )
+                                        })
         else:
-            logs[self.user_id] = {self.session_id : [
-                    {'role':'USER', 'message': user_input},
-                    {'role':'CHATBOT', 'message': chat_output},
-                    {'role':'SYSTEM', 'current_time':current_time}
-                ]}
+            logs[self.user_id] = {self.session_id : self.log_message(user_input,
+                                                                     chat_output,
+                                                                     current_time
+                                                                     )}
 
         with open(self.log_file_path, 'w', encoding='utf-8') as f:
             json.dump(logs, f, ensure_ascii=False, indent=4)
