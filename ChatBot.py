399d24d161df8348623c9ac69cde764351406398
jasonheesanglee <volvstang@gmail.com>
2024-06-17 13:51:10 +0900

    feat: updating gpt

--- ChatBot.py
+++ ChatBot.py
@@ -1,6 +1,6 @@
 import json
 import pytz
-import cohere
+# import cohere
 import streamlit as st
 from langchain_cohere import ChatCohere
 from langchain_openai import ChatOpenAI
@@ -95,7 +95,7 @@ class ChatBot:
         elif self.mode=='chatgpt':
             prompt = '\n'.join(self.get_chat_history()) + user_input
             prompt = prompt + f"\n{self.system_message.replace('{time}', current_time)}"
-            response = self.llm.chat(prompt).content
+            response = self.llm.invoke(prompt).content
 
 
         self.logger.log(user_input=user_input,
