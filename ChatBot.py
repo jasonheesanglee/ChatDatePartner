4804b2530362b0414dee2565f6e92df8c85e0ef0
jasonheesanglee <volvstang@gmail.com>
2024-06-18 10:10:42 +0900

    feat: fixing errors

--- ChatBot.py
+++ ChatBot.py
@@ -42,9 +42,10 @@ class ChatBot:
             if self.session_id in history[self.user_id]:
                 session_hist = history[self.user_id][self.session_id]
                 print(session_hist)
-                for user, chatbot, _ in session_hist:
+                for user, chatbot, time_log in session_hist:
                     chat_history.append(user)
                     chat_history.append(chatbot)
+                    chat_history.append(time_log)
         return chat_history
 
     def chat(self, user_input):  ## Cohere
